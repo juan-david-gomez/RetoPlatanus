4f0646d5176c9fcba07058168e41983031083613
Juan David Gomez <judagoes@gmail.com>
2017-03-30 13:53:47 -0500

    Mostrar los ultimos indicadores

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,6 +10,7 @@
 angular.module('bitcoinViewApp')
   .controller('MainCtrl', function ($scope,$websocket) {
     $scope.data = [];
+    $scope.dataActual = {};
 
     var dataStream = $websocket('wss://api.bitfinex.com/ws/v2');
 	var msg = JSON.stringify({ 
@@ -20,10 +21,22 @@ angular.module('bitcoinViewApp')
 
 
 	dataStream.onMessage(function(message) {
-	    $scope.data.push(JSON.parse(message.data));
+		var dataWs = JSON.parse(message.data);
+		if (dataWs[1] !== "hb") {
+	    	$scope.data.push(dataWs);
+	    	$scope.dataActual = dataWs;
+		}
 	});
 	dataStream.onOpen(function() {
 	    dataStream.send(msg);
 	});
+
+
+	$scope.getDiffPrice = function (obj) {
+		return obj[1] - obj[3];
+	};
+	$scope.getDiffSize = function (obj) {
+		return obj[2] - obj[4];
+	};
 
   });
